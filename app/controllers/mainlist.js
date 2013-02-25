2f148bd9ec1a4cca0476247dd9cd055a683b0496
freemem <freemember007@gmail.com>
2013-02-25 16:25:57 +0800

    mainlist

---
+++ app/controllers/mainlist.js
@@ -0,0 +1,28 @@
+var util = require("util");
+var user = Alloy.createModel("user",{email: 'freemem@163.com', password: '666666'});
+var feeds = Alloy.Collections.feed;
+
+//login method
+util.send('api/login', {email: user.get("email"), password: user.get("password")}, function(res){
+	var data = JSON.parse(res);
+	if(data.type == "success"){
+		items = data.items;
+		for(var i=0;i<items.length;i++){
+			var feed = Alloy.createModel("feed",{content:items[i].content, date:items[i].date, image:items[i].image});
+			//feed.save();
+			feeds.add(feed);
+		}
+	}else if(data.type == "fail"){
+		alert('用户名或密码错误！');
+	}else{
+		alert('unknown error');
+	}
+});
+
+user.save();
+
+function a(){
+	var feed = Alloy.createModel("feed",{content:"测试", date:"2013/02/25", image:"http://www.baidu.com/img/shouye_b5486898c692066bd2cbaeda86d74448.gif"});
+			//feed.save();
+	feeds.add(feed,{at: 0})
+}
---
+++ app/models/user.js
@@ -0,0 +1,36 @@
+exports.definition = {
+	
+	config: {
+		"columns": {
+			"email":"text",
+			"password":"text"
+		},
+		"adapter": {
+			"type": "properties",
+			"collection_name": "user"
+		}
+	},		
+
+	extendModel: function(Model) {		
+		_.extend(Model.prototype, {
+						
+			// extended functions go here
+
+		}); // end extend
+		
+		return Model;
+	},
+	
+	
+	extendCollection: function(Collection) {		
+		_.extend(Collection.prototype, {
+			
+			// extended functions go here			
+			
+		}); // end extend
+		
+		return Collection;
+	}
+		
+}
+
